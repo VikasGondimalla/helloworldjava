9e64b1392f4aa34a7e9bd6a8fda7f376e0567da5
VikasGondimalla <100249840+VikasGondimalla@users.noreply.github.com>
2022-08-21 14:54:42 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 2:49");
+    System.out.println("Hello World - triggered by weebhook");
     }
 }
